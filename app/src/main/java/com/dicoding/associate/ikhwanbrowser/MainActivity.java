2815b0b80aea4bdd33a4f2aeb8214bed4c41f09c
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-10-27 15:27:23 +0400

    versi 2.0

---
+++ app/src/main/java/com/dicoding/associate/ikhwanbrowser/MainActivity.java
@@ -0,0 +1,31 @@
+package com.dicoding.associate.ikhwanbrowser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void picikanSoto (View view){
+        goToUrl ( "https://historiafactory.wordpress.com/2017/03/31/salahuddin-al-ayyubi-2/");
+    }
+
+    public void picikanSup (View view) {
+        goToUrl ( "https://www.visitmasjidalaqsa.com/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+}
